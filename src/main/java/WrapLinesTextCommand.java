faa0f84c4e60b730a777db1c84dc6ce695cde89a
Tri <triml@stud.ntnu.no>
2026-01-28 17:35:15 +0100

    Implemented the execute method in the WrapLinesTextCommand class.

--- src/main/java/WrapLinesTextCommand.java
+++ src/main/java/WrapLinesTextCommand.java
@@ -1,21 +1,53 @@
+/**
+ * This class is responsible to wrap each line in a given text
+ * and provides functionality to do so.
+ */
 public class WrapLinesTextCommand extends WrapTextCommand
 {
+  private String selection;
+
+
+  /**
+   * Constructor to initialize.
+   * @param opening the opening wrapper.
+   * @param end the end wrapper.
+   */
   public WrapLinesTextCommand(String opening, String end)
   {
     super(opening, end);
   }
 
+  /**
+   * Method to wrap each line.
+   * @param text the text to wrap.
+   * @return the result of the text.
+   */
   public String execute(String text){
     if (text==null||text.isBlank()){
       throw new IllegalArgumentException("Text cannot be null or blank");
     }
+    String[] lines = text.split("\n");
+    StringBuilder result = new StringBuilder();
 
+    for (int i=0; i<lines.length; i++) {
+      result.append(opening);
+      result.append(lines[i]);
+      result.append(end);
 
+      if (i<lines.length-1){
+        result.append("\n");
 
+      }
+    }
+    return result.toString();
   }
 
+  /**
+   * Getter for selection.
+   * @return the selection
+   */
   public String getSelection() {
-    return
+    return selection;
   }
 
 

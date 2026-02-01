006780f39fa43a6e343a6719f4fcde02a2313d0e
Tri <triml@stud.ntnu.no>
2026-02-01 11:18:42 +0100

    Implemented the ScriptTest class with both positive and negative tests.

---
+++ src/test/java/ScriptTest.java
@@ -0,0 +1,57 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ScriptTest {
+
+  /**
+   * Postive test for running script.
+   */
+  @Test
+  void execute_valid_runsScript() {
+    TextCommand c1 = new CapitalizeTextCommand();
+    TextCommand c2 = new WrapTextCommand("<p>","</p>");
+    ArrayList<TextCommand> commands = new ArrayList<>();
+    commands.add(c1);
+    commands.add(c2);
+    Script script = new Script(commands);
+    String result = script.execute("halla");
+    assertEquals("<p>Halla</p>", result);
+  }
+
+
+  /**
+   * Negative test for running script, null commands.
+   */
+
+  @Test
+  void execute_null_throwException() {
+    TextCommand c1 = new CapitalizeTextCommand();
+    TextCommand c2 = new WrapTextCommand("<p>","</p>");
+    ArrayList<TextCommand> commands = new ArrayList<>();
+    commands.add(c1);
+    commands.add(c2);
+    Script script = new Script(commands);;
+    assertThrows(IllegalArgumentException.class, () -> script.execute(null));
+  }
+
+  /**
+   * Negative test for running script, blank commands.
+   */
+
+  @Test
+  void execute_blank_throwException() {
+    TextCommand c1 = new CapitalizeTextCommand();
+    TextCommand c2 = new WrapTextCommand("<p>","</p>");
+    ArrayList<TextCommand> commands = new ArrayList<>();
+    commands.add(c1);
+    commands.add(c2);
+    Script script = new Script(commands);;
+    assertThrows(IllegalArgumentException.class, () -> script.execute(""));
+  }
+
+
+}

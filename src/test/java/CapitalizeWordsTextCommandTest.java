c1360d943737be3c05740bc5a3d6634adfa9dc4d
Tri <triml@stud.ntnu.no>
2026-02-03 12:56:12 +0100

    Implemented the CapitalizeWordsTextCommandTest class with both positive and negative tests.

---
+++ src/test/java/CapitalizeWordsTextCommandTest.java
@@ -0,0 +1,47 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+/**
+ * Test class for CapitalizeWordsText class that tests for both negative and positive test scenarios
+ * to verify expected behavior.
+ */
+class CapitalizeWordsTextCommandTest {
+
+
+  /**
+   * Positive test to check that every first character in every word gets upper cased.
+   */
+  @Test
+  void execute_valid_upperCaseCharWords() {
+    CapitalizeWordsTextCommand command = new CapitalizeWordsTextCommand();
+    String text = "hei bror hva skjer";
+    String result = command.execute(text);
+    assertEquals("Hei Bror Hva Skjer", result);
+  }
+
+
+  /**
+   * Negative test that check for throw when trying to execute a null value for text string.
+   */
+  @Test
+  void execute_invalid_null() {
+    CapitalizeWordsTextCommand command = new CapitalizeWordsTextCommand();
+    String result = null;
+    assertThrows(IllegalArgumentException.class, () -> command.execute(result));
+  }
+
+  /**
+   * Negative test that check for throw when trying to execute a blank string.
+   *
+   */
+  @Test
+  void execute_invalid_empty() {
+    CapitalizeWordsTextCommand command = new CapitalizeWordsTextCommand();
+    String result = "";
+    assertThrows(IllegalArgumentException.class, () -> command.execute(result));
+  }
+
+
+}
